7687153ff40ab654f8f701cd4a814654536a9ccf
feliciovcm <feliciovcm@gmail.com>
2021-07-18 20:38:22 -0300

    Create component Can, to verify user permission and roles

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
 import { useCan } from "../hooks/useCan";
 import { setupAPIclient } from "../services/api";
@@ -7,10 +8,6 @@ import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const userCanSeeMetrics = useCan({
-    roles: ["editor"],
-  });
-
   useEffect(() => {
     api
       .get("/me")
@@ -22,7 +19,9 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email} </h1>
-      {userCanSeeMetrics && <div>Métricas</div>}
+      <Can permissions={["metrics.create"]}>
+        <div>Métricas</div>
+      </Can>
     </>
   );
 }
